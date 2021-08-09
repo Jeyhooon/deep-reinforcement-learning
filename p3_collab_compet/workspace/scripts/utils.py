4e72199b7538f3cb5861454fc596e46546426627
jeyhooon <jeyhoon.maskani@gmail.com>
2021-08-09 15:13:43 +0200

    added new files

---
+++ p3_collab_compet/workspace/scripts/utils.py
@@ -0,0 +1,68 @@
+
+import numpy as np
+import torch
+
+
+# from https://github.com/songrotek/DDPG/blob/master/ou_noise.py
+class OUNoise:
+    """
+    This strategy implements the Ornstein-Uhlenbeck process, which adds
+    time-correlated noise to the actions taken by the deterministic policy.
+    The OU process satisfies the following stochastic differential equation:
+    dxt = theta*(mu - xt)*dt + sigma*dWt
+    where Wt denotes the Wiener process
+
+    """
+
+    def __init__(self, action_dimension, scale=0.1, mu=0, theta=0.15, sigma=0.2):
+        self.action_dimension = action_dimension
+        self.scale = scale
+        self.mu = mu
+        self.theta = theta
+        self.sigma = sigma
+        self.state = np.ones(self.action_dimension) * self.mu
+        self.reset()
+
+    def reset(self):
+        self.state = np.ones(self.action_dimension) * self.mu
+
+    def noise(self):
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(len(x))
+        self.state = x + dx
+        return torch.tensor(self.state * self.scale).float()
+
+
+# https://github.com/ikostrikov/pytorch-ddpg-naf/blob/master/ddpg.py#L11
+def soft_update(target, source, tau):
+    """
+    Perform DDPG soft update (move target params toward source based on weight
+    factor tau)
+    Inputs:
+        target (torch.nn.Module): Net to copy parameters to
+        source (torch.nn.Module): Net whose parameters to copy
+        tau (float, 0 < x < 1): Weight factor for update
+    """
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
+
+
+# https://github.com/ikostrikov/pytorch-ddpg-naf/blob/master/ddpg.py#L15
+def hard_update(target, source):
+    """
+    Copy network parameters from source to target
+    Inputs:
+        target (torch.nn.Module): Net to copy parameters to
+        source (torch.nn.Module): Net whose parameters to copy
+    """
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(param.data)
+
+
+def transpose_list(mylist):
+    return list(map(list, zip(*mylist)))
+
+
+def transpose_to_tensor(input_list):
+    make_tensor = lambda x: torch.tensor(x, dtype=torch.float)
+    return list(map(make_tensor, zip(*input_list)))
